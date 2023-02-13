c4d249135d1be1f7955e9c1ef209577c33da160e
Christian Terron <75289330+A00828146@users.noreply.github.com>
2023-02-13 10:09:42 -0600

    Names added

--- Operaciones.js
+++ Operaciones.js
@@ -16,3 +16,20 @@ function calculate() {
 
     document.getElementById("resultado").innerHTML = result;
   }
+
+function calculateComplex() {
+  var n1 = parseFloat(document.getElementById("n1").value);
+  var n2 = parseFloat(document.getElementById("n2").value);
+  var op = document.getElementById("operador").value;
+  var result;
+
+  if (op == "Cuadrado") {
+    result = n1 * n1;
+  } else if (op == "Raiz") {
+    result = Math.sqrt(n1);
+  } else if (op == "Potencia") {
+    result = Math.pow(n1, n2);
+  } 
+
+  document.getElementById("resultado").innerHTML = result;
+}
